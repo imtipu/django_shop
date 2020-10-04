54637c428919a63d65ffd5d27d50d37260dd0948
Borhan Tipu <me@imtipu.me>
2020-10-04 06:11:21 +0600

    req added

--- users/models.py
+++ users/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 # Create your models here.
 from django_countries.fields import CountryField
-from phone_field import PhoneField
+# from phone_field import PhoneField
 
 
 class User(AbstractUser):
-    mobile_number = PhoneField(blank=True, help_text='Contact phone number')
+    mobile_number = models.CharField(blank=True, max_length=17, help_text='Contact phone number')
 
     class Meta:
         ordering = ('-date_joined',)
@@ -21,7 +21,7 @@ class CustomerBillingAddress(models.Model):
     road_no = models.CharField(max_length=32, null=True, blank=True)
     address_line1 = models.CharField(max_length=120, null=False)
     address_line2 = models.CharField(max_length=120, null=True, blank=True)
-    mobile_number = PhoneField(blank=True, help_text='Contact phone number')
+    mobile_number = models.CharField(blank=True, max_length=17, help_text='Contact phone number')
 
     city = models.ForeignKey('shop.City', related_name='billing_city', on_delete=models.CASCADE, null=False,
                              help_text='Billing City')
@@ -40,7 +40,7 @@ class CustomerShippingAddress(models.Model):
     road_no = models.CharField(max_length=32, null=True, blank=True)
     address_line1 = models.CharField(max_length=120, null=False)
     address_line2 = models.CharField(max_length=120, null=True, blank=True)
-    mobile_number = PhoneField(blank=True, help_text='Contact phone number')
+    mobile_number = models.CharField(blank=True, max_length=17, help_text='Contact phone number')
 
     city = models.ForeignKey('shop.City', related_name='shipping_city', on_delete=models.CASCADE, null=False,
                              help_text='Billing City')
@@ -51,3 +51,8 @@ class CustomerShippingAddress(models.Model):
     postal_code = models.CharField(max_length=20, null=False, help_text='Postal Code')
 
 
+class ChatRoom(models.Model):
+    user_one = models.ForeignKey('users.User', related_name='user_one_room', on_delete=models.CASCADE, null=False)
+    user_two = models.ForeignKey('users.User', related_name='user_two_room', on_delete=models.CASCADE, null=False)
+
+    
